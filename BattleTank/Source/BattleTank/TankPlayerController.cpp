ad9db40cb6f3220f68224adafedcc20283961625
bSlope8348 <bslope8348@gmail.com>
2020-07-04 23:52:45 -0600

    BT32-137 Finding Screen Pixel Coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -37,7 +37,15 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) cons
 {
 	//Get world location of linetrace through crosshair
 	//if hits world use out parameter for hit location
-	OutHitLocation = { 0, 1, 2 }; //Out parameter
+	
+	//Find the cross hair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	//"De-project" the screen position of the crosshair to a world direction
+	//Line-trace along that look direction, and see what we hit (up to max range)
+	
 	
 	//If hit landscape return true
 	//else return false
@@ -53,7 +61,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation;
 	if (GetSightRayHitLocation(HitLocation)) //Has "side-effect", is going to line trace
 	{	
-		UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *HitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Look Location: %s"), *ScreenLocation.ToString());
 	}
 	//if does not hit world
 	else
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,7 +15,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
@@ -25,10 +25,15 @@ public:
 
 	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
 	void AimTowardsCrosshair();
-
-private:
+	
 	//Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	// Crosshair location
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333;
 
 	//Return hit for first physics body in reach
 	//const FHitResult GetFirstPhysicsBodyInReach();
